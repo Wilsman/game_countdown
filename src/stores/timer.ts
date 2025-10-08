e76fb957344e788584af693b37c386f88c1e0b25
Wilsman <myleswilson77@hotmail.com>
2025-10-08 08:37:20 +0100

    softcore fix

--- src/stores/timer.ts
+++ src/stores/timer.ts
@@ -232,7 +232,7 @@ export const useTimerStore = defineStore("timer", () => {
     },
     {
       id: "tarkov-softcore",
-      title: "Escape from Tarkov Softcore(TBC)",
+      title: "Escape from Tarkov Softcore",
       titleColor: "#ffffff",
       targetDate: new Date("2025-10-08T09:00:00Z"), // Oct 8, 2025 8:00 AM (UTC)
       targetTimezone: "UTC",
